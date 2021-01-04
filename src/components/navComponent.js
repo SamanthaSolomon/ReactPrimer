2938811dbd87b0f95e4f5df0e9718bfce597b4c4
SamanthaSolomon <69916016+SamanthaSolomon@users.noreply.github.com>
2021-01-03 19:34:02 -0500

    nav component

---
+++ src/components/navComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Store</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
